9cd1735dcac4df2214f3fee4c563ed5c05ee78c0
pstanislavsky <pa.stan.ol@mail.ru>
2026-08-13 22:12:00 +0300

    Refactor dropdown component

--- components/common/dropdown_selector.py
+++ components/common/dropdown_selector.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import (
     ElementNotInteractableException,
 )
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 from base.components.base_component import BaseComponent
 from utils.xpath import has_class, has_text
@@ -9,25 +10,28 @@ from utils.xpath import has_class, has_text
 
 class DropdownSelector(BaseComponent):
     # Locators
-    DROPDOWN_LOCATOR = (By.XPATH, f'.//button[{has_class("dropdown-toggle")}]')
+    SELECT_LOCATOR = (By.XPATH, f'.//select')
+    TOGGLE_LOCATOR = (By.XPATH, f'.//*[@data-bs-toggle = "dropdown"]')
     MENU_LOCATOR = (By.XPATH, f'.//ul[{has_class("dropdown-menu")}]')
 
     def _get_option_locator(self, option):
         return (
             By.XPATH,
-            f'{self.MENU_LOCATOR[1]}'
-            f'//button[@data-action = "set-option"]'
-            f'[.//span[{has_text(option)}]]',
+            f'{self.MENU_LOCATOR[1]}' f'//li//*[{has_text(option)}]',
         )
 
     # Properties
     @property
     def is_enabled(self):
-        return self.get_element(self.DROPDOWN_LOCATOR, timeout=1).is_enabled()
+        return self.get_element(self.TOGGLE_LOCATOR, timeout=1).is_enabled()
 
     @property
     def selected_option(self):
-        return self.get_text(self.DROPDOWN_LOCATOR)
+        selected_option = Select(
+            self.get_present_element(self.SELECT_LOCATOR)
+        ).first_selected_option
+
+        return selected_option.get_property('textContent').strip()
 
     # Actions
     def select_option(self, option):
@@ -36,7 +40,7 @@ class DropdownSelector(BaseComponent):
                 f'Dropdown control is disabled and option "{option}" cannot be selected.'
             )
 
-        self.click_element(self.DROPDOWN_LOCATOR)
+        self.click_element(self.TOGGLE_LOCATOR)
         self.get_element(self.MENU_LOCATOR)
 
         if not self.get_element(self._get_option_locator(option)).is_enabled():
