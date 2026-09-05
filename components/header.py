8b6d2fe9ef4300027a190f9521670ba6c2ebb961
pstanislavsky <pa.stan.ol@mail.ru>
2026-09-05 17:43:01 +0300

    Remove authorization logic

--- components/header.py
+++ components/header.py
@@ -10,15 +10,6 @@ class Header(BaseComponent):
 
     # Locators
     CATALOG_MENU_LOCATOR = (By.XPATH, f'.//li[{has_class("catalog-menu")}]')
-    LOGIN_BUTTON_LOCATOR = (
-        By.XPATH,
-        f'.//a[{has_class("user-profile__login-link--desktop")}]',
-    )
-    PROFILE_BUTTON_LOCATOR = (
-        By.XPATH,
-        f'.//a[{has_class("user-profile__link--desktop")}]',
-    )
-    FAVORITES_BUTTON_LOCATOR = (By.XPATH, f'.//a[{has_class("favorites-link")}]')
     CART_BUTTON_LOCATOR = (By.XPATH, './/a[@data-entity = "header-cart"]')
     CART_COUNTER_LOCATOR = (By.XPATH, './/span[@data-block-id = "counter"]')
 
@@ -29,10 +20,6 @@ class Header(BaseComponent):
 
     # Properties
     @property
-    def is_logged_in(self):
-        return self.is_visible(self.PROFILE_BUTTON_LOCATOR)
-
-    @property
     def cart_counter(self):
         if self.is_visible(self.CART_COUNTER_LOCATOR):
             return int(self.get_text(self.CART_COUNTER_LOCATOR))
@@ -40,8 +27,5 @@ class Header(BaseComponent):
             return 0
 
     # Actions
-    def open_login_modal(self):
-        self.click_element(self.LOGIN_BUTTON_LOCATOR)
-
     def open_cart(self):
         self.click_element(self.CART_BUTTON_LOCATOR)
